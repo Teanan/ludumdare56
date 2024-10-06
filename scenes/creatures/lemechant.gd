2910392a075647894c852314ee105804d99d3d58
FoxyHawk <wolfyhawk.insalan@gmail.com>
2024-10-06 17:12:23 +0200

    chore(creature): fix lint for legentil and le mechant script

--- scenes/creatures/lemechant.gd
+++ scenes/creatures/lemechant.gd
@@ -12,11 +12,11 @@ extends RigidBody3D
 	#local_mat.blend_mode = BaseMaterial3D.BLEND_MODE_MUL
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
+func _on_area_3d_body_entered(_body: Node3D) -> void:
 	$DeathParticles.emitting = true
 
 	var free_timer = Timer.new()

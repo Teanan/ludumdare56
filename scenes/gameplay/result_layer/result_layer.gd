c453d6c65e7c766dfa00022231dd266f6251eb25
FoxyHawk <wolfyhawk.insalan@gmail.com>
2024-10-07 20:29:34 +0200

    feat(ui): fix result button

--- scenes/gameplay/result_layer/result_layer.gd
+++ scenes/gameplay/result_layer/result_layer.gd
@@ -19,7 +19,7 @@ func defeat_show() -> void:
 func result_show() -> void:
 	$ColorRect.visible = true
 	$MarginContainer.visible = true
-	get_tree().paused = true
+	$MarginContainer/Control/VBoxOptions.grab_focus()
 
 
 func result_hide() -> void:
@@ -33,7 +33,7 @@ func _on_next_pressed() -> void:
 		"show_progress_bar": true,
 		"level_name": "Level" + str(current_level + 1)
 	}
-	#Game.change_scene_to_file("res://scenes/gameplay/gameplay.tscn", params)
+	Game.change_scene_to_file("res://scenes/gameplay/gameplay.tscn", params)
 
 
 func _on_retry_pressed() -> void:
@@ -42,9 +42,9 @@ func _on_retry_pressed() -> void:
 		"show_progress_bar": true,
 		"level_name": "Level" + str(current_level)
 	}
-	#Game.change_scene_to_file("res://scenes/gameplay/gameplay.tscn", params)
+	Game.change_scene_to_file("res://scenes/gameplay/gameplay.tscn", params)
 
 
 func _on_main_menu_pressed() -> void:
 	print("back 2 menu")
-	#Game.change_scene_to_file("res://scenes/menu/menu.tscn", {"show_progress_bar": false})
+	Game.change_scene_to_file("res://scenes/menu/menu.tscn", {"show_progress_bar": false})
